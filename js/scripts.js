1b226647b3f3771e5903e0fb9cc31ca0ce062641
Sam Toohey <hello@samtoohey.xyz>
2021-07-13 14:59:39 +1000

    Started Player Modal
    player modal for user input

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,2 @@
-add_action( 'admin_init', 'redirect_non_logged_users_to_specific_page' );
 
-redirect_non_logged_users_to_specific_page = () => {
-
-if ( !is_user_logged_in() && is_page('add page slug or ID here') && $_SERVER['PHP_SELF'] != '/wp-admin/admin-ajax.php' ) {
-
-wp_redirect( 'https://https://campaign-initiative-tracker.netlify.app/login.html' ); 
-    exit;
-   }
-}
-
-
-const pwaAuth = document.querySelector("pwa-auth");
-pwaAuth.addEventListener("signin-completed", ev => {
-    const signIn = ev.detail;
-    if (signIn.error) {
-        console.error("Sign in failed", signIn.error);
-    } else {
-        console.log("Email: ", signIn.email);
-        console.log("Name: ", signIn.name);
-        console.log("Picture: ", signIn.imageUrl);
-        console.log("Provider (MS, Google, FB): ", signIn.provider);
-        console.log("Raw data from provider: ", signIn.providerData);
-    }
-});
 
